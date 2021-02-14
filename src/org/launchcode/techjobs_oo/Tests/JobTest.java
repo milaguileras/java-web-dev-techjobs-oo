c569d4d547ab0221f4d5182ab58c8e8d7c8a8902
Camila <camila@Matthews-MacBook-Pro-2.local>
2021-02-14 14:45:12 -0600

    added testSettingJobId and testJobConstructorSetsAllFields to tests

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,35 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.Location;
+import org.launchcode.techjobs_oo.Employer;
+import org.launchcode.techjobs_oo.CoreCompetency;
+import org.launchcode.techjobs_oo.PositionType;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    Job test1;
+    Job test2;
+    Job test3;
+
+    @Before
+    public void JobObject(){
+        test1 = new Job();
+        test2 = new Job();
+        test3= new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertEquals(1,test2.getId() - test1.getId() );
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        assertTrue(test3 instanceof Job);
+    }
+}
